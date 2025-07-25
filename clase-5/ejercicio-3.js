c59a4f0f56a68baa491c29a59d914f474e0d8d95
Diaz Martin <elmartind17@gmail.com>
2025-07-25 11:21:53 -0300

    Termine de hacer los ejercicios de la clase-5

---
+++ clase-5/ejercicio-3.js
@@ -0,0 +1,10 @@
+let salir = 3;
+let opcionIngresada = parseInt(
+  prompt("1- Consultar Saldo \n 2- Realizar extraccion \n 3- Salir")
+);
+
+do {
+  opcionIngresada = parseInt(
+    prompt("1- Consultar Saldo \n 2- Realizar extraccion \n 3- Salir")
+  );
+} while (opcionIngresada != 3);
